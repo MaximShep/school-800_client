a1655e48aceec8ab7cc12befd32ceed43933afaa
neon2k21 <roman_2001@icloud.com>
2024-04-16 11:20:47 +0300

    гзвфеу-10

---
+++ components/groupRatingListItem.js
@@ -0,0 +1,62 @@
+import { useEffect } from "react"
+import { View, Text } from "react-native"
+import ip_address from "../config"
+
+
+export default function groupRatingListItem(props){
+
+    const {fio, points,grp } = props
+    const [grp_image, setgrp_Image] = useState("")
+
+    const getImageOfGroup=()=>{
+        try {
+            var myHeaders = new Headers();
+            myHeaders.append("Content-Type", "application/json");
+           
+            var raw = JSON.stringify({
+               "grp_id" : grp,
+            });
+            var requestOptions = {
+              method: 'POST',
+              headers: myHeaders,
+              body: raw,
+              redirect: 'follow'
+            };
+            
+            fetch(ip_address+'/getImageOfGroup', requestOptions)
+              .then( response => response.json())
+              .then( result => {
+                console.log(result)
+                setgrp_Image(result)
+            })
+              .catch(error => console.log('error', error));
+        } catch (error) {
+          console.error(error);
+        }
+    }
+
+    useEffect(()=>{
+        getImageOfGroup()
+    },[])
+
+
+    return(
+
+        <View>
+            <Text>
+               {fio}
+            </Text>
+            <Text>
+                {points} баллов 
+            </Text>
+            <Text>
+               {grp_image} 
+            </Text>
+        </View>
+
+    )
+
+
+
+
+}
---
+++ components/individualTaskCard.js
@@ -0,0 +1,79 @@
+import { useEffect } from "react"
+import { View, Text, TouchableOpacity } from "react-native"
+import ip_address from "../config"
+import { useNavigation } from "@react-navigation/core"
+
+
+export default function individualTaskCard(props){
+
+    const {id, name, description, date_of_creation, date_of_deadline, point, track, image } = props
+    const [trackName, setTrackName] = useState("")
+    const {navigate} = useNavigation()
+
+    const getNameOfTrack=()=>{
+        try {
+            var myHeaders = new Headers();
+            myHeaders.append("Content-Type", "application/json");
+           
+            var raw = JSON.stringify({
+               "track_id" : track,
+            });
+            var requestOptions = {
+              method: 'POST',
+              headers: myHeaders,
+              body: raw,
+              redirect: 'follow'
+            };
+            
+            fetch(ip_address+'/getTrackName', requestOptions)
+              .then( response => response.json())
+              .then( result => {
+                console.log(result)
+                setTrackName(result)
+            })
+              .catch(error => console.log('error', error));
+        } catch (error) {
+          console.error(error);
+        }
+    }
+
+    useEffect(()=>{
+        getNameOfTrack()
+    },[])
+
+    return(
+
+        <TouchableOpacity
+        onPress={()=>{
+           navigate('Карточка задания');
+           global.task_id = id;
+           global.task_name = name;
+           global.task_description = description;
+           global.task_date_of_creation = date_of_creation;
+           global.task_date_of_deadline = date_of_deadline;
+           global.task_point = point;
+           global.task_track_name = trackName
+           global.task_image = image
+
+           }}>
+            <Text>
+               {name}
+            </Text>
+            <Text>
+                {point} баллов 
+            </Text>
+            <Text>
+               {trackName}
+            </Text>
+            <Text>
+                {image}
+            </Text>
+        </TouchableOpacity>
+
+    )
+
+
+
+
+
+}
